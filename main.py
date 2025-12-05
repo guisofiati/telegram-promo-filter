e853a5c9cba0236bef60dc4de4b859ae3a724100
Guilherme Sofiati Fantin <gui_sf@hotmail.com>
2025-12-04 22:13:48 -0300

    feat: mention users from list when sent message to channel

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 import os
+
+from Demos.win32ts_logoff_disconnected import username
 from dotenv import load_dotenv
 import logging
 
@@ -26,7 +28,8 @@ telethon_session_name = os.getenv('TELETHON_SESSION_NAME')
 
 upstash_url = os.getenv('UPSTASH_REDIS_REST_URL')
 upstash_token = os.getenv('UPSTASH_REDIS_REST_TOKEN')
-upstash_key = os.getenv('UPSTASH_KEY')
+upstash_key_words = os.getenv('UPSTASH_KEY_WORDS')
+upstash_key_mentions = os.getenv('UPSTASH_KEY_MENTIONS')
 
 session_hash = os.getenv('SESSION_HASH')
 
@@ -46,6 +49,7 @@ channels_listening = [
 ]
 
 words_listening = []
+users_mentions = []
 
 already_sent_message_empty_list_to_user = False
 
@@ -69,11 +73,13 @@ async def get_channel_id():
 async def main():
     try:
         global words_listening
+        global users_mentions
 
         await client_telegram.start()
 
         r = Redis(url=upstash_url, token=upstash_token)
-        words_listening = r.smembers(upstash_key)
+        words_listening = r.smembers(upstash_key_words)
+        users_mentions = r.smembers(upstash_key_mentions)
 
         logger.info('[UPSTASH] Initialized.')
         logger.info('[CLIENT_TELETHON] Client started successfully.')
@@ -97,19 +103,23 @@ async def main():
                     return
 
                 text = event.raw_text
-                text_formated = event.raw_text.replace("\n", " ")
+#               text_formated = event.raw_text.replace("\n", " ")
 
 #                logger.info(f'[CLIENT_TELETHON] Text: {text}')
 #                logger.info(f'[CLIENT_TELETHON] Text formated: {text_formated}')
 
                 if any(word.lower() in text.lower() for word in words_listening):
-                    logger.info(f'[CLIENT_TELETHON] A word matches from list. Sending message to whatsapp...')
+    #               logger.info(f'[CLIENT_TELETHON] A word matches from list. Sending message to whatsapp...')
+                    logger.info(f'A word matches from list. Sending message...')
 #                    client_twilio.messages.create(
 #                        to=twilio_to_phone,
 #                        from_=twilio_from_phone,
 #                        body=text
 #                    )
                     await client_telegram.send_message(destination_channel, message=text)
+                    if users_mentions:
+                        mentions = ' '.join([f'@{user}' for user in users_mentions])
+                        await client_telegram.send_message(destination_channel, message=mentions)
                     logger.info(f'[CLIENT_TELETHON] Message sent successfully')
             except Exception as err:
                 logger.error(f'Error: {err}')
@@ -120,12 +130,38 @@ async def main():
                 words += f"{i}. {word}\n"
             return f"WORDS LISTENING:\n\n{words}"
 
-        @client_telegram.on(events.NewMessage(from_users="me", chats="me", pattern=r"^/l"))
+        def get_users_mention() -> str:
+            mentions = ""
+            for i, mention in enumerate(users_mentions, start=1):
+                mentions += f"{i}. {mention}\n"
+            return f"USERS TO MENTION:\n\n{mentions}"
+
+        @client_telegram.on(events.NewMessage(from_users="me", chats=[destination_channel], pattern=r"^/l"))
         async def list_words(event):
             words = get_words_listening()
             await event.reply(words)
 
-        @client_telegram.on(events.NewMessage(from_users="me", chats="me", pattern=r"^/r(?:\s+(.*))?"))
+        @client_telegram.on(events.NewMessage(from_users="me", chats=[destination_channel], pattern=r"^/m"))
+        async def list_mentions(event):
+            mentions = get_users_mention()
+            await event.reply(mentions)
+
+        @client_telegram.on(events.NewMessage(from_users="me", chats=[destination_channel], pattern=r"^/a\s+(.*)"))
+        async def add_word(event):
+            try:
+                word = event.pattern_match.group(0)[3:].strip()
+                if len(word) <= 30:
+                    r.sadd(upstash_key_words, word)
+                    words_listening.append(word)
+                    logger.info(f"Word {word} added to listener")
+                    await event.reply(f"Word {word} added to listener.\n{get_words_listening()}")
+                else:
+                    logger.warning(f"Phrase or word too long")
+                    await event.reply(f"Phrase or word too long")
+            except Exception as err:
+                logger.error(f'Error adding word: {err}')
+
+        @client_telegram.on(events.NewMessage(from_users="me", chats=[destination_channel], pattern=r"^/r\s+(.*)"))
         async def remove_word(event):
             try:
                 arg = event.pattern_match.group(1)
@@ -133,7 +169,7 @@ async def main():
                     num_item = int(arg)
                     if 1 <= num_item <= len(words_listening):
                         item = words_listening[num_item-1]
-                        r.srem(upstash_key, item)
+                        r.srem(upstash_key_words, item)
                         words_listening.pop(num_item-1)
                         logger.info(f"Word {item} removed from listener.")
                         await event.reply(f"Word {item} removed from listener.\n{get_words_listening()}")
@@ -146,20 +182,41 @@ async def main():
             except Exception as err:
                 logger.error(f'Error removing word: {err}')
 
-        @client_telegram.on(events.NewMessage(from_users="me", chats="me", pattern=r"^/a(?:\s+(.*))?"))
-        async def add_word(event):
+        @client_telegram.on(events.NewMessage(from_users="me", chats=[destination_channel], pattern=r"^/am\s+(.*)"))
+        async def add_mention(event):
             try:
-                word = event.pattern_match.group(0)[3:].strip()
-                if len(word) <= 30:
-                    r.sadd(upstash_key, word)
-                    words_listening.append(word)
-                    logger.info(f"Word {word} added to listener")
-                    await event.reply(f"Word {word} added to listener.\n{get_words_listening()}")
+                user_mention = event.pattern_match.group(1).strip().lstrip('@')
+                if 0 < len(user_mention) <= 32:
+                    r.sadd(upstash_key_mentions, user_mention)
+                    users_mentions.append(user_mention)
+                    logger.info(f"User {user_mention} added to mention")
+                    await event.reply(f"User {user_mention} added to mention.\n{get_users_mention()}")
                 else:
-                    logger.warning(f"Phrase or word too long")
-                    await event.reply(f"Phrase or word too long")
+                    logger.warning(f"User {user_mention} too long")
+                    await event.reply(f"User {user_mention} too long")
             except Exception as err:
-                logger.error(f'Error adding word: {err}')
+                logger.error(f'Error adding user to mention: {err}')
+
+        @client_telegram.on(events.NewMessage(from_users="me", chats=[destination_channel], pattern=r"^/rm\s+(.*)"))
+        async def remove_mention(event):
+            try:
+                arg = event.pattern_match.group(1)
+                if arg.isdigit():
+                    num_item = int(arg)
+                    if 1 <= num_item <= len(users_mentions):
+                        item = users_mentions[num_item-1]
+                        r.srem(upstash_key_mentions, item)
+                        users_mentions.pop(num_item-1)
+                        logger.info(f"User {item} removed from mention.")
+                        await event.reply(f"User {item} removed from mention.\n{get_users_mention()}")
+                    else:
+                        logger.warning(f"Item index {arg} not found in users to mention")
+                        await event.reply(f"Item index {arg} not found in users to mention")
+                else:
+                    logger.warning("Error. You must pass a number from the list above. i.e: /rm 2")
+                    await event.reply("Error. You must pass a number from the list above. i.e: /rm 2")
+            except Exception as err:
+                logger.error(f'Error removing user to mention: {err}')
 
         await client_telegram.run_until_disconnected()
     except Exception as e:
